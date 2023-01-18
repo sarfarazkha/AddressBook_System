bfe6a7433098c8729a1f0f6a5d0d6bc7abb94256
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-18 22:47:42 +0530

    Added: Ability to add new contact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,22 +4,12 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
 
-        AddressBook obj = new AddressBook();
-        obj.setFirstName("sarfaraz");
-        obj.setLastName("khan");
-        obj.setCity("nagpur");
-        obj.setAddress("Hosa Yallapur Hosa Oni");
-        obj.setEmail("abc@123");
-        obj.setState("Maharashtra");
-        obj.setZip(580001);
-        obj.setPhone(9999999);
-        System.out.println(obj.getFirstName());
-        System.out.println(obj.getLastName());
-        System.out.println(obj.getPhone());
-        System.out.println(obj.getEmail());
-        System.out.println(obj.getAddress());
-        System.out.println(obj.getCity());
-        System.out.println(obj.getState());
-        System.out.println(obj.getZip());
+        System.out.println("Welcome to address book program");
+
+        AddressBook a1 = new AddressBook();
+        a1.newContact();
+        a1.newContact();
+
+        System.out.println(a1.list);
     }
 }
